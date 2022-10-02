7505936ec00f1cf72b5865af697845779cea4b8b
Gabriel Aquino <gabriel.aquino86@gmail.com>
2022-10-02 19:42:49 -0300

    Módulo 02 - Exercício 04.

---
+++ Modulo 02/Aula 04/RadarRodoviario/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace RadarRodoviario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Radar radar = new Radar();
+
+            Console.WriteLine("Informe a frequência do carro: ");
+            radar.FrequenciaCarro = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Informe a velocidade limite da estrada: ");
+            radar.VelocidadeLimite = Convert.ToInt32(Console.ReadLine());
+
+            radar.CalcularVelocidadeCarro();
+            Console.WriteLine("Velocidade do carro: " + radar.VelocidadeCarro);
+
+            if (radar.VelocidadeCarro > radar.VelocidadeLimite)
+            {
+                Console.WriteLine("Valor da Multa: " + radar.CalcularMulta().ToString());
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Modulo 02/Aula 04/RadarRodoviario/Radar.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RadarRodoviario
+{
+    public class Radar
+    {
+        #region Propriedades
+        public double FrequenciaCarro { get; set; }
+
+        public int VelocidadeLimite { get; set; }
+
+        public double VelocidadeCarro { get; set; }
+        #endregion
+
+        #region Métodos
+        public void CalcularVelocidadeCarro()
+        {
+            VelocidadeCarro = 300 * (1 / FrequenciaCarro) - 1;
+        }
+
+        public double CalcularMulta()
+        {
+            double multa;
+            double vintePerc = VelocidadeLimite * 0.2;
+            double limite20perc = VelocidadeLimite + vintePerc;           
+
+            if (VelocidadeCarro > VelocidadeLimite && VelocidadeCarro <= limite20perc)
+            {                
+                multa = 127.69;
+                return multa;
+            }
+
+            if (VelocidadeCarro > limite20perc)
+            {               
+                multa = 574.62;
+                return multa;
+            }
+
+            return 0;
+        }
+        #endregion
+    }
+}
