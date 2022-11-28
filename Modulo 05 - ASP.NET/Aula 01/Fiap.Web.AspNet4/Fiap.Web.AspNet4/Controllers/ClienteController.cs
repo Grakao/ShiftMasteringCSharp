74b1a5880c2c4d5c95135999f3927963bf3e74f7
Gabriel Aquino <gabriel.aquino86@gmail.com>
2022-11-28 09:50:29 -0300

    Modulo 05 - Aula 01.

---
+++ Modulo 05 - ASP.NET/Aula 01/Fiap.Web.AspNet4/Fiap.Web.AspNet4/Controllers/ClienteController.cs
@@ -0,0 +1,31 @@
+﻿using Fiap.Web.AspNet4.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fiap.Web.AspNet4.Controllers
+{
+    public class ClienteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Novo()
+        {
+            return View();
+            //return Content("Fiap ASP.NET 4");
+            //return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Novo(ClienteModel clienteModel)
+        {
+            Console.WriteLine(clienteModel.Nome);
+            Console.WriteLine(clienteModel.Sobrenome);
+
+            // ClasseBancoDeDados.Insert (nome, sobrenome);
+            return View("Sucesso");
+        }
+    }
+}
