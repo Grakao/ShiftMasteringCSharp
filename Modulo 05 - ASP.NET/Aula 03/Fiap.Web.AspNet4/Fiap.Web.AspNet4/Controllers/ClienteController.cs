76a0cab6a1cc91b0a05f7c0312074643053bdddb
Gabriel Aquino <aquino-g@nsk.com>
2022-11-30 20:30:49 -0300

    Modulo 05 - Aula 03.

---
+++ Modulo 05 - ASP.NET/Aula 03/Fiap.Web.AspNet4/Fiap.Web.AspNet4/Controllers/ClienteController.cs
@@ -0,0 +1,200 @@
+﻿using Fiap.Web.AspNet4.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fiap.Web.AspNet4.Controllers
+{
+    public class ClienteController : Controller
+    {
+        public IActionResult Index()
+        {
+            var listaClientes = new List<ClienteModel>();
+            listaClientes.Add(new ClienteModel
+            {
+                ClienteId = 1,
+                Nome = "Flávio",
+                Sobrenome = "Moreni",
+                Email = "fmoreni@gmail.com",
+                DataNascimento = DateTime.Now,
+                Observacao = "OBS1"
+            });
+
+            listaClientes.Add(new ClienteModel
+            {
+                ClienteId = 2,
+                Nome = "Geinha",
+                Sobrenome = "Assanhada",
+                Email = "geinha@gmail.com",
+                DataNascimento = DateTime.Now,
+                Observacao = "OBS1"
+            });
+
+            listaClientes.Add(new ClienteModel
+            {
+                ClienteId = 3,
+                Nome = "Vanuncias",
+                Sobrenome = "Boldrera",
+                Email = "vanuncias@gmail.com",
+                DataNascimento = DateTime.Now,
+                Observacao = "OBS1"
+            });
+
+            listaClientes.Add(new ClienteModel
+            {
+                ClienteId = 4,
+                Nome = "Clersio",
+                Sobrenome = "Souto",
+                Email = "clersio@gmail.com",
+                DataNascimento = DateTime.Now,
+                Observacao = "OBS1"
+            });
+
+            //ViewBag.Clientes = listaClientes;
+            //ViewData["Clientes"] = listaClientes;
+            //TempData["Clientes"] = listaClientes;
+            return View(listaClientes);
+        }
+
+        [HttpGet]
+        public IActionResult Novo()
+        {
+            return View();
+            //return Content("Fiap ASP.NET 4");
+            //return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Novo(ClienteModel clienteModel)
+        {
+            Console.WriteLine(clienteModel.Nome);
+            Console.WriteLine(clienteModel.Sobrenome);
+
+            // ClasseBancoDeDados.Insert (nome, sobrenome);		
+
+            TempData["Mensagem"] = "Cliente cadastrado com sucesso!";
+
+            //return View("Sucesso");
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Editar(int id)
+        {
+            // var cliente = SELECT ... FROM CLIENTE WHERE ID = {id}; 
+
+            var clienteModel = new ClienteModel();
+            if (id == 1)
+            {
+                clienteModel = new ClienteModel
+                {
+                    ClienteId = 1,
+                    Nome = "Flavio",
+                    Email = "fmoreni@gmail.com",
+                    DataNascimento = DateTime.Now,
+                    Observacao = "OBS1"
+                };
+            }
+            else if (id == 2)
+            {
+                clienteModel = new ClienteModel
+                {
+                    ClienteId = 2,
+                    Nome = "Eduardo",
+                    Email = "eduardo@gmail.com",
+                    DataNascimento = DateTime.Now,
+                    Observacao = "OBS3"
+                };
+            }
+            else
+            {
+                clienteModel = new ClienteModel
+                {
+                    ClienteId = 3,
+                    Nome = "Moreni",
+                    Email = "moreni@gmail.com",
+                    DataNascimento = DateTime.Now,
+                    Observacao = "OBS3"
+                };
+            }
+            return View(clienteModel);
+        }
+
+        [HttpPost]
+        public IActionResult Editar(ClienteModel clienteModel)
+        {
+            // Validação de campos no Controller usando if/else recuperando dados da model
+            //if (String.IsNullOrEmpty(clienteModel.Nome) || String.IsNullOrEmpty(clienteModel.Email))
+            //{
+            //    TempData["Mensagem"] = "O nome do cliente ou o e-mail não pode ser nulo!";
+            //    return View(clienteModel);
+            //    //return RedirectToAction("Editar", new { id = clienteModel.ClienteId });
+            //}
+            //else
+            //{
+            //    // UPDATE TABELA ... VALUES ...
+            //    TempData["Mensagem"] = "Cliente alterado com sucesso!";
+            //    return RedirectToAction("Index");
+            //}
+
+            if (ModelState.IsValid)
+            {
+                // UPDATE TABELA ... VALUES ...
+                TempData["Mensagem"] = "Cliente alterado com sucesso!";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(clienteModel);
+            }
+
+            
+        }
+
+        [HttpGet]
+        public IActionResult Remover(int id)
+        {
+            TempData["Mensagem"] = "Cliente removido com sucesso!";
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Detalhe(int id)
+        {
+            var clienteModel = new ClienteModel();
+            if (id == 1)
+            {
+                clienteModel = new ClienteModel
+                {
+                    ClienteId = 1,
+                    Nome = "Flavio",
+                    Email = "fmoreni@gmail.com",
+                    DataNascimento = DateTime.Now,
+                    Observacao = "OBS1"
+                };
+            }
+            else if (id == 2)
+            {
+                clienteModel = new ClienteModel
+                {
+                    ClienteId = 2,
+                    Nome = "Eduardo",
+                    Email = "eduardo@gmail.com",
+                    DataNascimento = DateTime.Now,
+                    Observacao = "OBS3"
+                };
+            }
+            else
+            {
+                clienteModel = new ClienteModel
+                {
+                    ClienteId = 3,
+                    Nome = "Moreni",
+                    Email = "moreni@gmail.com",
+                    DataNascimento = DateTime.Now,
+                    Observacao = "OBS3"
+                };
+            }
+            return View(clienteModel);
+        }
+    }
+}
---
+++ Modulo 05 - ASP.NET/Aula 03/Fiap.Web.AspNet4/Fiap.Web.AspNet4/Models/ClienteModel.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Fiap.Web.AspNet4.Models
+{
+	public class ClienteModel
+	{
+		[HiddenInput]
+		public int ClienteId { get; set; }
+
+        [Display(Name = "Nome do Cliente")]
+        [Required(ErrorMessage = "O nome do cliente é obrigatório!")]
+		[MinLength(6,ErrorMessage = "São necessários pelo menos 6 caracteres para o nome do cliente.")]
+        [MaxLength(70, ErrorMessage = "Limite máximo de 70 caracteres para o nome do cliente.")]
+        public string? Nome { get; set; }
+
+        [Display(Name = "Sobrenome do Cliente")]
+        public string? Sobrenome { get; set; }
+
+        [Display(Name = "Endereço de E-mail")]
+        [Required(ErrorMessage = "O Email do cliente é obrigatório!")]
+		[EmailAddress(ErrorMessage = "Digite um endereço de e-mail válido.")]
+        public string? Email { get; set; }
+
+		[Display(Name = "Data de Nascimento")]
+		[Required(ErrorMessage = "A data de nascimento é obrigatória!")]
+		[DataType(DataType.Date, ErrorMessage="Data de nascimento incorreta.")]
+		public DateTime DataNascimento { get; set; }
+
+        [Display(Name = "Observações")]
+        public string? Observacao { get; set; }
+	}
+}
